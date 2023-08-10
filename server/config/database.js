04edfed227a620202872f84d1d660b41ab45dbbd
obenari <obenari@g.jct.ac.il>
2023-08-10 19:08:24 +0300

    1

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "e315439554",
+  password: "211378658",
   database: "appointments",
 });
 
